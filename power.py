85976fe93385bb77eacd70cbece576f11c814b6e
Deepa Pandey <deepapandey364@gmail.com>
2020-07-16 20:49:29 +0530

    jul16

---
+++ power.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        res = math.pow(x,n)
+        return res
