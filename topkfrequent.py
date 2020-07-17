2172e75c8295a4479789a348f5f5039ea37c06b8
Deepa Pandey <deepapandey364@gmail.com>
2020-07-17 22:04:58 +0530

    jul17

---
+++ topkfrequent.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        frequency = {}
+        a=0
+        ls=[]
+        for i in nums:
+            if i in frequency:
+                frequency[i]+= 1
+            else:
+                frequency[i] = 1
+        for w in sorted(frequency, key=frequency.get, reverse=True):
+            ls.append(w)
+            
+        return ls[0:k]
